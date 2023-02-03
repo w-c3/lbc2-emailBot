28809c9ac0eeb4d3316cb3638acd4468cbb6ecaa
dk42506 <94137106+dk42506@users.noreply.github.com>
2023-02-03 18:57:57 -0500

    Added more features
    Have to make sure to change PDF file path from DEsktop to Downloads

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import tkinter as tk
 import win32com.client as win32
+import re
 
 def submit():
+    senderEmail = entry.get()
+    sender = senderEmail.split("@")[0]
     school = entry1.get()
     principalEmail = entry2.get()
     name = entry3.get()
-    gradClass = entry4.get()
+    gradClass = re.split(":|0", sender, maxsplit=1)[-1].strip()
     principalName = entry5.get()
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = str(principalEmail)
     mail.cc = "cmhale2023@loyolablakefield.org"
     mail.Subject = 'LBC2 Invite'
-    mail.Body = "Dear " + str(principalName) + ",\nMy name is " + str(name) + " and I am a student at Loyola Blakefield High School. I am a leader of Loyola Blakefield’s cybersecurity program, and I would love to invite your students at the " + str(school) + " to our student-created cybersecurity competition, LBC2.\nLBC2 is an incredible opportunity for students interested in computers and cybersecurity, and absolutely no prior experience is required. The best part? The competition is entirely free! We would love to have some of your students attend! There will be many adult and student mentors in both in-person and virtual environments to assist our competitors and make the day a real learning experience. We will offer various prizes to winners, as well as breakfast and lunch offered to those participating. I have attached the flyer with more information about the competition, which is on March 25th, 2023, from 9:00am to 3:00pm on Loyola Blakefield’s campus or virtually. Registration for in-person attendance ends March 23rd and for virtual ends at the start of the competition. Register here at our website: https://lbc2.org/\nIf you have any questions at all, please don’t hesitate to reach out. Loyola Blakefield Cyber is excited to extend our outreach and education to __ this year! Thank you for your time.\n\nBest,\n" + str(name) + "\nClass of " + str(gradClass)
+    mail.Body = "Dear " + str(principalName) + ",\nMy name is " + str(name) + " and I am a student at Loyola Blakefield High School. I am a leader of Loyola Blakefield’s cybersecurity program, and I would love to invite your students at the " + str(school) + " to our student-created cybersecurity competition, LBC2.\nLBC2 is an incredible opportunity for students interested in computers and cybersecurity, and absolutely no prior experience is required. The best part? The competition is entirely free! We would love to have some of your students attend! There will be many adult and student mentors in both in-person and virtual environments to assist our competitors and make the day a real learning experience. We will offer various prizes to winners, as well as breakfast and lunch offered to those participating. I have attached the flyer with more information about the competition, which is on March 25th, 2023, from 9:00am to 3:00pm on Loyola Blakefield’s campus or virtually. Registration for in-person attendance ends March 23rd and for virtual ends at the start of the competition. Register here at our website: https://lbc2.org/\nIf you have any questions at all, please don’t hesitate to reach out. Loyola Blakefield Cyber is excited to extend our outreach and education to __ this year! Thank you for your time.\n\nBest,\n" + str(name) + "\nClass of '" + str(gradClass)
 
-    # To attach a file to the email (optional):
-    attachment  = "C:/Users/dekeller2024/Desktop/LBC2_2023 (1).pdf"
+    attachment  = "C:/Users/" + str(sender) + "/Desktop/LBC2_2023.pdf"
+    #attachment  = "https://www.rossnazirullah.com/students/images/Rubiks.pdf"
     mail.Attachments.Add(attachment)
 
     mail.Send()
@@ -27,6 +30,12 @@ root = tk.Tk()
 root.title("Simple Input Application")
 root.geometry("800x600")
 
+
+label = tk.Label(root, text="Your Email:")
+label.pack()
+
+entry = tk.Entry(root)
+entry.pack()
 
 label1 = tk.Label(root, text="School:")
 label1.pack()
@@ -45,13 +54,13 @@ label3.pack()
 
 entry3 = tk.Entry(root)
 entry3.pack()
-
+"""
 label4 = tk.Label(root, text="Graduation Class:")
 label4.pack()
 
 entry4 = tk.Entry(root)
 entry4.pack()
-
+"""
 label5 = tk.Label(root, text="Name of Principal (ex. Ms. Smith):")
 label5.pack()
 
