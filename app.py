6b1fc00b8caf1b548f30b736b766e25f9c365cb2
dk42506 <94137106+dk42506@users.noreply.github.com>
2023-02-03 18:57:27 -0500

    Update 1
    Added more variables for text

--- app.py
+++ app.py
@@ -4,19 +4,24 @@ import win32com.client as win32
 def submit():
     school = entry1.get()
     principalEmail = entry2.get()
+    name = entry3.get()
+    gradClass = entry4.get()
+    principalName = entry5.get()
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = str(principalEmail)
-    mail.cc = 'cmhale2023@loyolablakefield.org'
+    mail.cc = "cmhale2023@loyolablakefield.org"
     mail.Subject = 'LBC2 Invite'
-    mail.Body = "Dear " + str(school) + ",\nMy name is Dylan Keler and I am a Junior at Loyola Blakefield High School. I am a leader of Loyola Blakefield’s cybersecurity program, and I would love to invite your students at " + str(school) + " to our student-created cybersecurity competition, LBC2.\nLBC2 is an incredible opportunity for students interested in computers and cybersecurity, and absolutely no prior experience is required. The best part? The competition is entirely free! We would love to have some of your students attend! There will be many adult and student mentors in both in-person and virtual environments to assist our competitors and make the day a real learning experience. We will offer various prizes to winners, as well as breakfast and lunch offered to those participating. I have attached the flyer with more information about the competition, which is on March 25th, 2023, from 9:00am to 3:00pm on Loyola Blakefield’s campus or virtually. Registration for in-person attendance ends March 23rd and for virtual ends at the start of the competition. Register here at our website: https://lbc2.org/\nIf you have any questions at all, please don’t hesitate to reach out. Loyola Blakefield Cyber is excited to extend our outreach and education you this year! Thank you for your time.\n\nBest,\nDylan Keller\nClass of 24"
+    mail.Body = "Dear " + str(principalName) + ",\nMy name is " + str(name) + " and I am a student at Loyola Blakefield High School. I am a leader of Loyola Blakefield’s cybersecurity program, and I would love to invite your students at the " + str(school) + " to our student-created cybersecurity competition, LBC2.\nLBC2 is an incredible opportunity for students interested in computers and cybersecurity, and absolutely no prior experience is required. The best part? The competition is entirely free! We would love to have some of your students attend! There will be many adult and student mentors in both in-person and virtual environments to assist our competitors and make the day a real learning experience. We will offer various prizes to winners, as well as breakfast and lunch offered to those participating. I have attached the flyer with more information about the competition, which is on March 25th, 2023, from 9:00am to 3:00pm on Loyola Blakefield’s campus or virtually. Registration for in-person attendance ends March 23rd and for virtual ends at the start of the competition. Register here at our website: https://lbc2.org/\nIf you have any questions at all, please don’t hesitate to reach out. Loyola Blakefield Cyber is excited to extend our outreach and education to __ this year! Thank you for your time.\n\nBest,\n" + str(name) + "\nClass of " + str(gradClass)
 
     # To attach a file to the email (optional):
     attachment  = "C:/Users/dekeller2024/Desktop/LBC2_2023 (1).pdf"
     mail.Attachments.Add(attachment)
 
     mail.Send()
-    print("Complete")
+    successText = tk.Label(root, text="Successful email sent to " + str(school))
+    successText.pack()
+    print("Successful email sent to " + str(school))
 
 root = tk.Tk()
 root.title("Simple Input Application")
@@ -34,6 +39,24 @@ label2.pack()
 
 entry2 = tk.Entry(root)
 entry2.pack()
+
+label3 = tk.Label(root, text="Your Name:")
+label3.pack()
+
+entry3 = tk.Entry(root)
+entry3.pack()
+
+label4 = tk.Label(root, text="Graduation Class:")
+label4.pack()
+
+entry4 = tk.Entry(root)
+entry4.pack()
+
+label5 = tk.Label(root, text="Name of Principal (ex. Ms. Smith):")
+label5.pack()
+
+entry5 = tk.Entry(root)
+entry5.pack()
 
 submit_button = tk.Button(root, text="Submit", command=submit)
 submit_button.pack()
